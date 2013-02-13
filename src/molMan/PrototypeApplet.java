bfdb362dc23244ea680ad36bfd8011bd0f78dee6
jbkstuff <jbkstuff@gmail.com>
2013-02-13 10:54:48 -0600

    Fixed white spaces and other stuff...

--- src/molMan/PrototypeApplet.java
+++ src/molMan/PrototypeApplet.java
@@ -14,37 +14,29 @@ import org.jmol.api.JmolSimpleViewer;
 //The applet code
 public class PrototypeApplet extends Applet {
 
-	private static final long serialVersionUID = 1L;
-	
-	JmolSimpleViewer viewer;
+	private static final long serialVersionUID = 1L;	
+	JmolSimpleViewer viewer0;
+	JmolSimpleViewer viewer1;
     String structurePbd;
-    JmolPanel jmolPanel;
-    JmolPanel jmolPanel2;
+    JmolPanel jmolPanel0;
+    JmolPanel jmolPanel1;
     private final int W = 1400;
     private final int H = 800;
 	
 
 	public void init()
 	{
-   
-		
-		
-		jmolPanel = new JmolPanel();
-        jmolPanel2 = new JmolPanel();
+		//Calculate the appropriate size for jmolPanel		
+		int jmolWidth = this.getWidth()/3;
+				
+		jmolPanel0 = new JmolPanel();
+        jmolPanel1 = new JmolPanel();
         
-        jmolPanel.setPreferredSize(new Dimension(300,300));
-        jmolPanel2.setPreferredSize(new Dimension(300, 300));
-        setStructure();
+        jmolPanel0.setPreferredSize(new Dimension(jmolWidth,jmolWidth));
+        jmolPanel1.setPreferredSize(new Dimension(jmolWidth, jmolWidth));
+        loadStructure();		
 		
-		/*JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		
-		JButton button = new JButton("This is a button...");
-		
-		panel.add(button, BorderLayout.WEST);
-		panel.add(jmolPanel, BorderLayout.EAST);
-		this.add(panel);*/
-                //sets up main applet window
+        //sets up main applet window
         this.setPreferredSize(new Dimension(W,H));
         //sets up layout of main window
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -56,6 +48,7 @@ public class PrototypeApplet extends Applet {
         top.setLayout(new BoxLayout(top, BoxLayout.X_AXIS));
         middle.setLayout(new BoxLayout(middle, BoxLayout.X_AXIS));
         bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+        
         //logo for the program
         JPanel logo = new JPanel();
         JLabel logoLabel = new JLabel();
@@ -71,7 +64,7 @@ public class PrototypeApplet extends Applet {
         //text box
         JPanel text = new JPanel();
         text.setLayout(new FlowLayout());
-        JTextField input = new JTextField(50);
+        JTextField input = new JTextField(30);
         text.add(input);
         //draw button
         JPanel button = new JPanel();
@@ -80,7 +73,7 @@ public class PrototypeApplet extends Applet {
         button.add(draw);
         //add elements to the top pane
         
-        top.setAlignmentY(CENTER_ALIGNMENT);
+        //this.setAlignmentY(CENTER_ALIGNMENT);
         top.add(logo);
         //top.add(Box.createRigidArea(new Dimension(50, 150)));
         top.add(mol);
@@ -104,19 +97,21 @@ public class PrototypeApplet extends Applet {
         tabs.addTab("Reset", null, res, "View GPA here");
         
         //adds tabbed display to the middle of the layout
+        JPanel molViewer0 = new JPanel();
         JPanel molViewer1 = new JPanel();
-        JPanel molViewer2 = new JPanel();
+        molViewer0.setLayout(new FlowLayout());
         molViewer1.setLayout(new FlowLayout());
-        molViewer2.setLayout(new FlowLayout());
-        molViewer1.add(jmolPanel);
-        molViewer2.add(jmolPanel2);
-        middle.add(Box.createRigidArea(new Dimension(5, 300)));
+        molViewer0.add(jmolPanel0);
+        molViewer1.add(jmolPanel1);
+        //middle.add(Box.createRigidArea(new Dimension(5, 300)));
         middle.add(tabs);
-        middle.add(Box.createRigidArea(new Dimension(10, 300)));
+        //middle.add(Box.createRigidArea(new Dimension(10, 300)));
+        middle.add(molViewer0);
+        //middle.add(Box.createRigidArea(new Dimension(10, 300)));
         middle.add(molViewer1);
-        middle.add(Box.createRigidArea(new Dimension(10, 300)));
-        middle.add(molViewer2);
-        middle.add(Box.createRigidArea(new Dimension(20, 300)));
+        //middle.add(Box.createRigidArea(new Dimension(20, 300)));
+        
+        
         //create previous/next buttons
         JButton previous = new JButton("Previous");
         JButton next = new JButton("Next");
@@ -126,37 +121,38 @@ public class PrototypeApplet extends Applet {
         JTextField out = new JTextField(45);
         out.setEditable(false);
         //set up bottom layout
-        bottom.add(Box.createRigidArea(new Dimension(400, 100)));
+        bottom.add(Box.createRigidArea(new Dimension(300, 100)));
         bottom.add(previous);
         bottom.add(molVar);
         bottom.add(next);
         bottom.add(out);
         //adds each section to applet window
-        this.add(Box.createRigidArea(new Dimension(W, 50)));
+        //this.add(Box.createRigidArea(new Dimension(W, 50)));
         this.add(top);
-        this.add(Box.createRigidArea(new Dimension(W, 20)));
+        //this.add(Box.createRigidArea(new Dimension(W, 20)));
         this.add(middle);
-        this.add(Box.createRigidArea(new Dimension(W, 5)));
+        //this.add(Box.createRigidArea(new Dimension(W, 5)));
         this.add(bottom);
-        this.add(Box.createRigidArea(new Dimension(W, 2)));
+        //this.add(Box.createRigidArea(new Dimension(W, 2)));
 	}
 	
-	public void setStructure() 
+	public void loadStructure() 
 	{
 		 
-        JmolSimpleViewer viewer = jmolPanel.getViewer();
- 
-        viewer.openFile("5PTI.pdb");
- 
-        // send the PDB file to Jmol.
-        // there are also other ways to interact with Jmol, but they require more
-        // code. See the link to SPICE above...
-        //viewer.openStringInline(pdb);
-        viewer.evalString("select *; spacefill off; wireframe off; backbone 0.4;  ");
-        viewer.evalString("color chain;  ");
-        //viewer.evalString("rotate on;");
+        JmolSimpleViewer view0 = jmolPanel0.getViewer();
+        JmolSimpleViewer view1 = jmolPanel1.getViewer();
         
-        this.viewer = viewer; 
+ 
+        view0.openFile("5PTI.pdb");
+        view1.openFile("5PTI.pdb");
+        
+        //viewer.evalString("select *; spacefill off; wireframe off; backbone 0.4;  ");
+        //viewer.evalString("color chain;  ");
+        view0.evalString("rotate on;");
+        view1.evalString("rotate on;");
+        
+        this.viewer0 = view0; 
+        this.viewer1 = view1;
     }
 	
 	
@@ -170,10 +166,10 @@ public class PrototypeApplet extends Applet {
         private static final long serialVersionUID = -3661941083797644242L;
         JmolSimpleViewer viewer;
         JmolAdapter adapter;
-        JmolPanel() {
+        JmolPanel() 
+        {
             adapter = new SmarterJmolAdapter();
             viewer = JmolSimpleViewer.allocateSimpleViewer(this, adapter);
- 
         }
  
         public JmolSimpleViewer getViewer() 
